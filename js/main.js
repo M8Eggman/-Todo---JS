acca8bc7f142143d56e289b508110d98c6515531
GoNEAR <cagatay.morgul@outlook.com>
2025-04-25 11:14:21 +0200

    ajout contenu dans mes éléments creer

--- js/main.js
+++ js/main.js
@@ -1,10 +1,18 @@
 // récupère le body
-let body = document.getElementsByTagName("body")[0]
+let body = document.getElementsByTagName("body")[0];
 
-// creation de tout les élément de base qui vont être injecté au début
-let newH1 = document.createElement("h1")
-let newInputText = document.createElement("input")
-let newInputButton = document.createElement("input")
-let buttonAll = document.createElement("button")
-let buttonFini = document.createElement("button")
-let buttonPasFini = document.createElement("button")
+// création de tout les élément de base qui vont être injecté au début
+let newH1 = document.createElement("h1");
+let newInputText = document.createElement("input");
+let newInputButton = document.createElement("input");
+let buttonAll = document.createElement("button");
+let buttonFini = document.createElement("button");
+let buttonPasFini = document.createElement("button");
+
+// changement du contenu des élément créer précedemment
+newH1.textContent = "To Do List";
+newInputText.placeholder = "Tâche";
+newInputButton.value = "Add";
+buttonAll.textContent = "All";
+buttonAll.textContent = "Fini";
+buttonAll.textContent = "Pas Fini";
