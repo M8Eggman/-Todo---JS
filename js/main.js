534eda91ef587f0d3ef27a666d2e8f0a78436dd8
GoNEAR <cagatay.morgul@outlook.com>
2025-04-25 12:10:31 +0200

    creation des elements necessaire a la creation dune tache

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 // mes imports
 import { body, newH1, newInputText, newInputButton, buttonAll, buttonFini, buttonPasFini, divInput, divButton, header, sectionControle, sectionToDoList } from "./modules/newElements.js";
 
-// changement du contenu des élément créer dans newElements
+// changement du contenu ou ajout d'attribut des élément créer dans newElements
 newH1.textContent = "To Do List";
 newInputText.type = "text";
 newInputText.placeholder = "Tâche";
@@ -10,28 +10,43 @@ newInputButton.value = "Add";
 buttonAll.textContent = "All";
 buttonFini.textContent = "Fini";
 buttonPasFini.textContent = "Pas Fini";
-sectionControle.id = "sectionControle"
-sectionToDoList.id = "sectionToDoList"
+sectionControle.id = "sectionControle";
+sectionToDoList.id = "sectionToDoList";
 divInput.id = "divInput";
 divButton.id = "divButton";
 
 // injection d'élément dans mon Body
-body.appendChild(header)
-body.appendChild(sectionControle)
-body.appendChild(sectionToDoList)
+body.append(header, sectionControle, sectionToDoList);
 
 // injection d'élément dans section controle
-sectionControle.appendChild(divInput);
-sectionControle.appendChild(divButton);
+sectionControle.append(divInput, divButton);
 
 // injection d'élément dans mon header
-header.appendChild(newH1);
+header.append(newH1);
 
 // injection d'élément dans ma div input
-divInput.appendChild(newInputText);
-divInput.appendChild(newInputButton);
+divInput.append(newInputText, newInputButton);
 
 // injection d'élément dans ma div button
-divButton.appendChild(buttonAll);
-divButton.appendChild(buttonFini);
-divButton.appendChild(buttonPasFini);
+divButton.append(buttonAll, buttonFini, buttonPasFini);
+
+// mes event listener
+// création de nouvelle tache au click du input add
+newInputButton.addEventListener("click", () => {
+  // si l'utilisateur n'a rien entré ou qu'il a entré que des espaces il n'y a pas de nouvelle tâche créé
+  if (newInputText.value.trim().length == 0) {
+    alert("Vous n'avez rien entré veuillez entré une tâche");
+  } else {
+    // création de tous les éléments nécessaire pour faire une nouvelle tâche
+    let newDivTache = document.createElement("div");
+    let newDivCheckbox = document.createElement("div");
+    let newDivOptions = document.createElement("div");
+    let checkboxLabel = document.createElement("label");
+    let checkboxInput = document.createElement("input");
+    let newP = document.createElement("p");
+    let newButtonModifier = document.createElement("button");
+    let newButtonSupprimer = document.createElement("button");
+
+ 
+  }
+});
--- js/modules/eventListener.js
+++
@@ -1,8 +0,0 @@
-// mes imports
-import { body, newH1, newInputText, newInputButton, buttonAll, buttonFini, buttonPasFini, divInput, divButton } from "./newElements.js";
-
-newInputButton.addEventListener("click", () => {
-    if (newInputText.value.length == 0) {
-        
-    }
-});
