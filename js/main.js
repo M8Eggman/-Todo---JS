7290270895e054108dc98f77e6d75b1010a98261
GoNEAR <cagatay.morgul@outlook.com>
2025-04-25 11:26:20 +0200

    injection des element dans body

--- js/main.js
+++ js/main.js
@@ -8,11 +8,31 @@ let newInputButton = document.createElement("input");
 let buttonAll = document.createElement("button");
 let buttonFini = document.createElement("button");
 let buttonPasFini = document.createElement("button");
+let divInput = document.createElement("div");
+let divButton = document.createElement("div");
 
 // changement du contenu des élément créer précedemment
 newH1.textContent = "To Do List";
+newInputText.type = "text";
 newInputText.placeholder = "Tâche";
+newInputButton.type = "button";
 newInputButton.value = "Add";
 buttonAll.textContent = "All";
-buttonAll.textContent = "Fini";
-buttonAll.textContent = "Pas Fini";
+buttonFini.textContent = "Fini";
+buttonPasFini.textContent = "Pas Fini";
+divInput.id = "divInput";
+divButton.id = "divButton";
+
+// injection des éléments dans mon Body
+body.appendChild(newH1);
+body.appendChild(divInput);
+body.appendChild(divButton);
+
+// injection des éléments dans ma div input
+divInput.appendChild(newInputText);
+divInput.appendChild(newInputButton);
+
+// injection des éléments dans ma div input
+divButton.appendChild(buttonAll)
+divButton.appendChild(buttonFini)
+divButton.appendChild(buttonPasFini)
