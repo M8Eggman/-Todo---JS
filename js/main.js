cf2643ec6a6dd02d11edb61b8ddc9f377a28f421
GoNEAR <cagatay.morgul@outlook.com>
2025-04-25 11:08:44 +0200

    creation de tout les élément de base qui vont être injecté au début

---
+++ js/main.js
@@ -0,0 +1,10 @@
+// récupère le body
+let body = document.getElementsByTagName("body")[0]
+
+// creation de tout les élément de base qui vont être injecté au début
+let newH1 = document.createElement("h1")
+let newInputText = document.createElement("input")
+let newInputButton = document.createElement("input")
+let buttonAll = document.createElement("button")
+let buttonFini = document.createElement("button")
+let buttonPasFini = document.createElement("button")
