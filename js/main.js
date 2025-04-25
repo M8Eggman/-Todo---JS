beab99ccf4646c5e9d80772e961d01e6224b2003
GoNEAR <cagatay.morgul@outlook.com>
2025-04-25 11:47:42 +0200

    ajout de section et de header qui englobe mes anciens element

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
-import { body, newH1, newInputText, newInputButton, buttonAll, buttonFini, buttonPasFini, divInput, divButton } from "./modules/newElements.js";
+// mes imports
+import { body, newH1, newInputText, newInputButton, buttonAll, buttonFini, buttonPasFini, divInput, divButton, header, sectionControle, sectionToDoList } from "./modules/newElements.js";
 
-// changement du contenu des élément créer précedemment
+// changement du contenu des élément créer dans newElements
 newH1.textContent = "To Do List";
 newInputText.type = "text";
 newInputText.placeholder = "Tâche";
@@ -9,19 +10,28 @@ newInputButton.value = "Add";
 buttonAll.textContent = "All";
 buttonFini.textContent = "Fini";
 buttonPasFini.textContent = "Pas Fini";
+sectionControle.id = "sectionControle"
+sectionToDoList.id = "sectionToDoList"
 divInput.id = "divInput";
 divButton.id = "divButton";
 
-// injection des éléments dans mon Body
-body.appendChild(newH1);
-body.appendChild(divInput);
-body.appendChild(divButton);
+// injection d'élément dans mon Body
+body.appendChild(header)
+body.appendChild(sectionControle)
+body.appendChild(sectionToDoList)
 
-// injection des éléments dans ma div input
+// injection d'élément dans section controle
+sectionControle.appendChild(divInput);
+sectionControle.appendChild(divButton);
+
+// injection d'élément dans mon header
+header.appendChild(newH1);
+
+// injection d'élément dans ma div input
 divInput.appendChild(newInputText);
 divInput.appendChild(newInputButton);
 
-// injection des éléments dans ma div input
+// injection d'élément dans ma div button
 divButton.appendChild(buttonAll);
 divButton.appendChild(buttonFini);
 divButton.appendChild(buttonPasFini);
---
+++ js/modules/eventListener.js
@@ -0,0 +1,8 @@
+// mes imports
+import { body, newH1, newInputText, newInputButton, buttonAll, buttonFini, buttonPasFini, divInput, divButton } from "./newElements.js";
+
+newInputButton.addEventListener("click", () => {
+    if (newInputText.value.length == 0) {
+        
+    }
+});
